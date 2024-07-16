26c8121e41d1d6219eb77e0f4514138c0f49f868
amirhossein <amirhosseinmobini9@gmail.com>
2024-07-16 16:17:42 +0330

    ✨feature: create match added

--- match/match.go
+++ match/match.go
@@ -1,9 +1,32 @@
 package match
 
+import "math/rand"
+
 type Match struct {
 	GameId  string
 	OPlayer string
 	XPlayer string
 	Turn    string
 	Moves   map[string]string
+}
+
+func New(gameId, firstPlayer string) *Match {
+	match := Match{
+		GameId: gameId,
+	}
+	if playerIcon() == "X" {
+		match.XPlayer = firstPlayer
+	} else {
+		match.OPlayer = firstPlayer
+	}
+	return &match
+}
+
+func playerIcon() string {
+	randNumber := rand.Int()
+	if randNumber%2 == 0 {
+		return "X"
+	} else {
+		return "O"
+	}
 }
