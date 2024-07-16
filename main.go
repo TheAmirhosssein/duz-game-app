4b4465a3fb88d7ba5919e54af2d23983952e72c1
amirhossein <amirhosseinmobini9@gmail.com>
2024-07-16 16:03:32 +0330

    ✨feature: simple websocket server created

--- main.go
+++ main.go
@@ -1,5 +1,42 @@
 package main
 
-func main() {
+import (
+	"log"
+	"net/http"
 
+	"github.com/gorilla/websocket"
+)
+
+var upgrader = websocket.Upgrader{
+	CheckOrigin: func(r *http.Request) bool {
+		return true
+	},
+}
+
+func echo(w http.ResponseWriter, r *http.Request) {
+	conn, err := upgrader.Upgrade(w, r, nil)
+	if err != nil {
+		log.Println("Upgrade:", err)
+		return
+	}
+	defer conn.Close()
+
+	for {
+		messageType, message, err := conn.ReadMessage()
+		if err != nil {
+			log.Println("Read:", err)
+			break
+		}
+		log.Printf("Received: %s", message)
+		err = conn.WriteMessage(messageType, message)
+		if err != nil {
+			log.Println("Write:", err)
+			break
+		}
+	}
+}
+
+func main() {
+	http.HandleFunc("/", echo)
+	log.Fatal(http.ListenAndServe("localhost:8080", nil))
 }
