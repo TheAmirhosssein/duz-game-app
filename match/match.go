93434c44dc3935c263fa24e4d6e4a4db38ed2fe9
amirhossein <amirhosseinmobini9@gmail.com>
2024-07-25 15:33:01 +0330

    🐞 fix: game over in column way bug fixed

--- match/match.go
+++ match/match.go
@@ -110,15 +110,17 @@ func (match *Match) CheckValidRemove(square string) bool {
 func (match Match) IsGameOverColumn() bool {
 	for counter := 1; counter <= 3; counter++ {
 		lastSign := match.Moves[fmt.Sprint(counter)]
-		gameOver := true
-		for squareIterator := counter; squareIterator <= counter+6; squareIterator += 3 {
-			if match.Moves[fmt.Sprint(squareIterator)] != lastSign {
-				gameOver = false
-				break
+		if lastSign != "" {
+			gameOver := true
+			for squareIterator := counter; squareIterator <= counter+6; squareIterator += 3 {
+				if match.Moves[fmt.Sprint(squareIterator)] != lastSign {
+					gameOver = false
+					break
+				}
 			}
-		}
-		if gameOver {
-			return true
+			if gameOver {
+				return true
+			}
 		}
 	}
 	return false
