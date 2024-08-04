58ddb5662a00781ee79e266a798c448e8f17ca30
amirhossein <amirhosseinmobini9@gmail.com>
2024-08-04 23:11:44 +0330

    ✨feature: send message added

--- messages/messages.go
+++ messages/messages.go
@@ -65,3 +65,9 @@ func ParseMessage(validKeys []string, message []byte) (map[string]string, error)
 	}
 	return result, nil
 }
+
+func GenerateMessage(messageType, userId, gameId string, message any) []byte {
+	generatedMessage := map[string]any{"type": messageType, "user_id": userId, "message": message, "game_id": gameId}
+	serializedMessage, _ := json.Marshal(generatedMessage)
+	return serializedMessage
+}
